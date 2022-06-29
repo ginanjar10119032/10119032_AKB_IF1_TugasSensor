45dbbc11a7e8c1359634a5fd864c662f2e1a25d2
ginanjartg <ginanjar.10119032@mahasiswa.unikom.ac.id>
2022-06-29 20:48:37 +0700

    first push

---
+++ app/src/main/java/com/example/tugassensorgenap2022akbif_110119032/SplashScreen.java
@@ -0,0 +1,39 @@
+package com.example.tugassensorgenap2022akbif_110119032;
+
+//NIM     : 10119032
+//NAMA    : GINANJAR TUBAGUS GUMILAR
+//KELAS   : IF-1
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){};
+        setContentView(R.layout.activity_splash_screen);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 3000L); //3000 L = 3 detik
+    }
+}
